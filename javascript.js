bd37ed3009461f83c110be817909983ed886e8dd
mbright90 <michael.bright90@icloud.com>
2022-07-13 11:04:49 +0100

    Creating password check function
    Creating a function which listens for key inputs on the confirm
    password input field and checks whether it matches the choose password
    input field. When the two passwords match exactly the field becomes
    valid.

--- javascript.js
+++ javascript.js
@@ -1,18 +1,50 @@
 let passwordInputs = document.querySelectorAll('input[type=password]');
+let choosePassword = document.querySelector('#choose-password');
+let confirmPassword = document.querySelector('#confirm-password');
+
 let passwordReq = document.querySelectorAll('li');
 
-console.log(passwordReq);
+// ****************************************************************** //
 
-function checkIsFocus(e) {
-    if (passwordInputs[0] == document.activeElement || passwordInputs[1] == document.activeElement) {
+function checkIsFocus() {
+    if (choosePassword == document.activeElement || confirmPassword == document.activeElement) {
         passwordReq.forEach(listItem => {
             listItem.style.setProperty('display', 'inherit', 'important'); // Showing the password requirements when user
         });                                                                // focus is on a password field
-    } else if (passwordInputs[0] != document.activeElement || passwordInputs[1] != document.activeElement) {
+    } else {
         passwordReq.forEach(listItem => {
             listItem.style.setProperty('display', 'none', 'important');    // Removes the password requirements when user
         });                                                                // focus is not ona  password field
     };
 };
 
+function checkPassword() {
+    
+};
+
 window.addEventListener('click', checkIsFocus);
+
+// ****************************************************************** //
+
+choosePassword.addEventListener('keyup', () => {                       // Still working on validating each password requirement to
+    passwordReq.forEach(requirement => {                               // ultimately change the color of the requirement to green when
+        regexPattern = new RegExp(`${requirement.dataset.regex}`);     // successfully meeting criteria
+        if (regexPattern.test(choosePassword.value == true)) {
+            requirement.style.color = '#d3eee5';
+        } else {
+            requirement.style.color = 'inherit';
+        }    
+    });
+});
+
+// ****************************************************************** //
+
+passwordInputs.forEach(passwordInput => {                             // Checks the validity of the confirmPassword input field to
+    passwordInput.addEventListener('keyup', () => {                   // match the choosePassword input field
+        if (choosePassword.value == confirmPassword.value) {
+            confirmPassword.setCustomValidity("");
+        } else {
+            confirmPassword.setCustomValidity("false");
+        }
+    });
+});
